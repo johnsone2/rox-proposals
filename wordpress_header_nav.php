3d80305f5a6ce87b81acfb0a2594625fb42d95c6
jonbullock <jon@talentriot.com>
2013-07-27 18:29:02 -0600

    Added php header/nav for wordpress

---
+++ wordpress_header_nav.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Rox Proposals</title>
+
+    <!--Rox Proposal bootstrap overrides-->
+    <link href="css/rox-proposals.css" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="js/jquery.js" type="text/javascript"></script>
+
+    <script>
+    </script>
+
+
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span3">
+
+        </div>
+        <div class="span9">
+            <div class="header">
+                <h3 class="text-muted">Rox Proposals</h3>
+            </div>
+        </div>
+    </div>
+    <div class="row-fluid">
+        <div class="span3">
+            <ul class="nav nav-pills nav-stacked" id="side-nav" >
+                <li class="active"><a href="">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Packages</a></li>
+                <li><a href="blog/">Blog</a></li>
+                <li><a href="#">Fairytale Endings</a></li>
+                <li><a href="#">Social</a></li>
+            </ul>
+        </div>
+        <div class="span9">
