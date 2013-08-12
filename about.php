e176fc3492f80b6f47c4a94cba6618565a84a350
jonbullock <jon@talentriot.com>
2013-08-12 17:43:13 -0600

    Switched to a single php file for header and footer content.

---
+++ about.php
@@ -0,0 +1,74 @@
+<?php include 'header_nav.php'; ?>
+
+<div class="span1"></div>
+
+<div class="span8">
+    <div>
+        <h1>Meet the Rox-star duo</h1>
+
+        <p class="lead">Keeping it in the family, we are Denver-native cousins that have a passion for event planning and love stories.
+            As our family has grown over the years, and with each proposal more creative than the last,  we have become increasingly eager to plan proposals and celebrate the milestone that is an engagement. Let us make your family just as excited about love as ours is, they’ll be talking about your proposal for years to come!</p>
+
+    </div>
+    <div class="row-fluid thumbnail light-background" style="margin-bottom: 20px;">
+
+        <div class="span3" style="text-align: center;">
+            <h3 >Christina</h3>
+            <img alt="Christina Forrest" title="Christina Forrest" src="img/stock/forrest.jpg">
+        </div>
+        <div class="span9">
+            <p class="caption">Detail oriented, organized, and charming, Christina thrives off of picture-perfect plans. Equipped with a business marketing degree from Ole Miss, this master planner is sure to surprise you with her southern side!  Christina has vast event  planning experience ranging from large scale formals to VIP events. When she is not creating memories, Christina loves walking her chocolate lab, skiing the Rocky Mountains, and organizing any and everything.</p>
+            <!--<p>-->
+            <!--<a href="#">-->
+            <!--<img alt="Facebook" title="Facebook" src="img/glyphicons_410_facebook.png"/>-->
+            <!--</a>-->
+            <!--<a href="#">-->
+            <!--<img alt="Twitter" title="Twitter" src="img/glyphicons_411_twitter.png"/>-->
+            <!--</a>-->
+            <!--<a href="#">-->
+            <!--<img alt="Instagram" title="Instagram" src="img/glyphicons_412_instagram.png"/>-->
+            <!--</a>-->
+            <!--<a href="#">-->
+            <!--<img alt="Pinterest" title="Pinterest" src="img/glyphicons_399_pinboard.png"/>-->
+            <!--</a>-->
+            <!--<a href="#">-->
+            <!--<img alt="LinkedIn" title="LinkedIn" src="img/glyphicons_397_linked_in.png"/>-->
+            <!--</a>-->
+            <!--</p>-->
+        </div>
+
+        <!--</div>-->
+    </div>
+
+    <div class="row-fluid thumbnail light-background">
+        <div class="span3" style="text-align: center;">
+            <h3>Larisa</h3>
+            <img alt="Larisa Barber" title="Larisa Barber" src="img/stock/barber.jpg">
+        </div>
+        <div class="span9">
+            <div class="caption" >
+                <p>With a passion for fashion and all things creative, Larisa’s ambition is derived from creating the perfect event. With experience in both the fashion and event planning industries, Larisa is able to successfully plan and execute events from celebrity affairs to intimate parties. In addition to experience, Larisa has a Bachelor’s degree in Fashion Merchandising Management and Economics from the Fashion Institute in New York City. Larisa loves to travel the world, especially Italy where she recently lived and looks for a new adventure everyday.</p>
+                <!--<p>-->
+                <!--<a href="#">-->
+                <!--<img alt="Facebook" title="Facebook" src="img/glyphicons_410_facebook.png"/>-->
+                <!--</a>-->
+                <!--<a href="#">-->
+                <!--<img alt="Twitter" title="Twitter" src="img/glyphicons_411_twitter.png"/>-->
+                <!--</a>-->
+                <!--<a href="#">-->
+                <!--<img alt="Instagram" title="Instagram" src="img/glyphicons_412_instagram.png"/>-->
+                <!--</a>-->
+                <!--<a href="#">-->
+                <!--<img alt="Pinterest" title="Pinterest" src="img/glyphicons_399_pinboard.png"/>-->
+                <!--</a>-->
+                <!--<a href="#">-->
+                <!--<img alt="LinkedIn" title="LinkedIn" src="img/glyphicons_397_linked_in.png"/>-->
+                <!--</a>-->
+                <!--</p>-->
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
