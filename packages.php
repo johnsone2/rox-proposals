6bb27ddd1565d6463565e67f906d0f71d610c0d3
Erik Johnson <johnsone3745@gmail.com>
2013-08-18 17:12:45 -0600

    switch packages to new style

--- packages.php
+++ packages.php
@@ -1,72 +1,130 @@
 <?php include 'header_nav.php'; ?>
-    <div>
-        <h1>Packages</h1>
 
-        <p class="lead">We pride our boutique style business on building close relationships with every person we work with.
-            Although there are many technologically advanced options out there to meet virtually, we still require face to face appointments in order to truly understand each and every client and their individual needs. </p>
+    <h1 style="text-align: center;">Packages</h1>
+    <div class="row-fluid">
+
+        <div id="cp" class="span4 transition-option" style="text-align: center;">
+            <img onclick="displayConceptionPlanning()" alt="Proposal Conception Planning" src="img/ring-on-finger.jpg"/>
+            <h4>Proposal Conception Planning</h4>
+        </div>
+        <div id="ep" class="span4 transition-option" style="text-align: center;">
+            <img onclick="displayEventPlanning()" alt="Proposal Event Planning" src="img/ring-on-finger.jpg"/>
+            <h4>Proposal Event Planning</h4>
+        </div>
+        <div id="pp" class="span4 transition-option" style="text-align: center;">
+            <img onclick="displayPremiumProposal()" alt="Premium Proposal" src="img/ring-on-finger.jpg"/>
+            <h4>Premium Proposal</h4>
+        </div>
+
+        <div id="pe" class="span11">
+            <p class="lead">We pride our boutique style business on building close relationships with every person we work with.
+                Although there are many technologically advanced options out there to meet virtually, we still require face to face appointments in order to truly understand each and every client and their individual needs.
+            </p>
+        </div>
+
+        <div id="cpi" class="span11" style="display: none;">
+            <p class="lead">This package is right for someone who wants to execute a plan on their own but needs a jump start with ideas.
+                A detailed question and answer session will be held, after which three personalized options will be presented.
+                Of course we will always be available for advice or questions as you plan your perfect proposal. </br></br>
+                Need more help after the proposal? A la carte items include ring shopping and post engagement party planning, click here to discover how our a la carte options can compliment your conception and event planning packages!
+            </p>
+        </div>
+
+        <div id="epi" class="span11" style="display: none;" >
+            <p class="lead">Looking for a middle ground with your proposal planning? Let us meet you <i>more than</i> halfway!
+                We will create proposal ideas, carry out proposal event coordination, and assist with the actual proposal execution.
+                We will be there every step of the way to ensure that your proposal lives up to the one your soon-to-be fiance&eacute; has dreamed of since she was a little girl (every girl does, no matter what they tell you)!
+                </br></br>
+                This package requires a meeting with you, your fiance's best friend, siblings, parents, and anyone that you think knows her best. This will help us paint the picture of what she envisions.
+                We’ll be sure to cover every aspect of the proposal that your bride expects for the big day before the BIG day!
+                </br></br>
+                We will be on site before, during, and after the proposal to execute it flawlessly, and make sure every vendor is in place. We ensure that our vendors will give you the best prices and capture each moment perfectly.
+                We also offer coaching and preparation for you in the days leading up to the event just incase you need some help calming those nerves.
+                </br></br>
+                Take the stress-free route and invest in the first biggest day of your life!
+                </br>
+                Prices will be customized depending on chosen proposal.
+            </p>
+        </div>
+
+        <div id="ppi" class="span11" style="display: none;">
+            <p class="lead">
+                Have absolutely no idea where to start or what your girlfriend wants? Let us do the investigating to create your unforgettable experience. Many times life and work get in the way of your perfect proposal, and that's where we come in.
+                All you have to do is pick a theme, we’ll do the rest- including ring selection help, and engagement party planning! This stress-free option is sure to end in a yes. Let us handle every aspect for you.
+                </br></br>
+                Themes: </br>
+            </p>
+            <ul>
+                <li>Traditional</li>
+                <li>Modern/New-Age</li>
+                <li>Adventurous</li>
+                <li>Exotic</li>
+            </ul>
+            <p>
+                Prices will be customized depending on venues, vendors and accommodations.
+                </br></br>
+                Take our customized proposal preference test, so we can determine what your bride has in mind!
+            </p>
+        </div>
 
     </div>
-    <div class="row-fluid" style="margin-bottom: 20px;">
-        <div class="span12">
-            <div class="media">
-                <div class="media-body light-background" style="">
-                    <h4 class="media-heading">Proposal Conception Planning</h4>
-                    <p>This package is right for someone who wants to execute a plan on their own but needs a jump start with ideas.
-                        A detailed question and answer session will be held, after which three personalized options will be presented.
-                        Of course we will always be available for advice or questions as you plan your perfect proposal. </br></br>
-                        Need more help after the proposal? A la carte items include ring shopping and post engagement party planning, click here to discover how our a la carte options can compliment your conception and event planning packages!
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row-fluid" style="margin-bottom: 20px;">
-        <div class="span12">
-            <div class="media">
-                <div class="media-body light-background">
-                    <h4 class="media-heading">Proposal Event Planning</h4>
-                    <p>Looking for a middle ground with your proposal planning? Let us meet you <i>more than</i> halfway!
-                        We will create proposal ideas, carry out proposal event coordination, and assist with the actual proposal execution.
-                        We will be there every step of the way to ensure that your proposal lives up to the one your soon-to-be fiance&eacute; has dreamed of since she was a little girl (every girl does, no matter what they tell you)!
-                        </br></br>
-                        This package requires a meeting with you, your fiance's best friend, siblings, parents, and anyone that you think knows her best. This will help us paint the picture of what she envisions.
-                        We’ll be sure to cover every aspect of the proposal that your bride expects for the big day before the BIG day!
-                        </br></br>
-                        We will be on site before, during, and after the proposal to execute it flawlessly, and make sure every vendor is in place. We ensure that our vendors will give you the best prices and capture each moment perfectly.
-                        We also offer coaching and preparation for you in the days leading up to the event just incase you need some help calming those nerves.
-                        </br></br>
-                        Take the stress-free route and invest in the first biggest day of your life!
-                        </br>
-                        Prices will be customized depending on chosen proposal.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row-fluid" style="margin-bottom: 20px;">
-        <div class="span12">
-            <div class="media">
-                <div class="media-body light-background">
-                    <h4 class="media-heading">Premium Proposal - The Unforgettable Package</h4>
-                    <p>
-                        Have absolutely no idea where to start or what your girlfriend wants? Let us do the investigating to create your unforgettable experience. Many times life and work get in the way of your perfect proposal, and that's where we come in.
-                        All you have to do is pick a theme, we’ll do the rest- including ring selection help, and engagement party planning! This stress-free option is sure to end in a yes. Let us handle every aspect for you.
-                        </br></br>
-                        Themes: </br>
-                    </p>
-                    <ul>
-                        <li>Traditional</li>
-                        <li>Modern/New-Age</li>
-                        <li>Adventurous</li>
-                        <li>Exotic</li>
-                    </ul>
-                    <p>
-                        Prices will be customized depending on venues, vendors and accommodations.
-                        </br></br>
-                        Take our customized proposal preference test, so we can determine what your bride has in mind!
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
+<!--    <div class="row-fluid" style="margin-bottom: 20px;">-->
+<!--        <div class="span12">-->
+<!--            <div class="media">-->
+<!--                <div class="media-body light-background" style="">-->
+<!--                    <h4 class="media-heading">Proposal Conception Planning</h4>-->
+<!---->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="row-fluid" style="margin-bottom: 20px;">-->
+<!--        <div class="span12">-->
+<!--            <div class="media">-->
+<!--                <div class="media-body light-background">-->
+<!--                    <h4 class="media-heading">Proposal Event Planning</h4>-->
+<!--                    <p>Looking for a middle ground with your proposal planning? Let us meet you <i>more than</i> halfway!-->
+<!--                        We will create proposal ideas, carry out proposal event coordination, and assist with the actual proposal execution.-->
+<!--                        We will be there every step of the way to ensure that your proposal lives up to the one your soon-to-be fiance&eacute; has dreamed of since she was a little girl (every girl does, no matter what they tell you)!-->
+<!--                        </br></br>-->
+<!--                        This package requires a meeting with you, your fiance's best friend, siblings, parents, and anyone that you think knows her best. This will help us paint the picture of what she envisions.-->
+<!--                        We’ll be sure to cover every aspect of the proposal that your bride expects for the big day before the BIG day!-->
+<!--                        </br></br>-->
+<!--                        We will be on site before, during, and after the proposal to execute it flawlessly, and make sure every vendor is in place. We ensure that our vendors will give you the best prices and capture each moment perfectly.-->
+<!--                        We also offer coaching and preparation for you in the days leading up to the event just incase you need some help calming those nerves.-->
+<!--                        </br></br>-->
+<!--                        Take the stress-free route and invest in the first biggest day of your life!-->
+<!--                        </br>-->
+<!--                        Prices will be customized depending on chosen proposal.-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="row-fluid" style="margin-bottom: 20px;">-->
+<!--        <div class="span12">-->
+<!--            <div class="media">-->
+<!--                <div class="media-body light-background">-->
+<!--                    <h4 class="media-heading">Premium Proposal - The Unforgettable Package</h4>-->
+<!--                    <p>-->
+<!--                        Have absolutely no idea where to start or what your girlfriend wants? Let us do the investigating to create your unforgettable experience. Many times life and work get in the way of your perfect proposal, and that's where we come in.-->
+<!--                        All you have to do is pick a theme, we’ll do the rest- including ring selection help, and engagement party planning! This stress-free option is sure to end in a yes. Let us handle every aspect for you.-->
+<!--                        </br></br>-->
+<!--                        Themes: </br>-->
+<!--                    </p>-->
+<!--                    <ul>-->
+<!--                        <li>Traditional</li>-->
+<!--                        <li>Modern/New-Age</li>-->
+<!--                        <li>Adventurous</li>-->
+<!--                        <li>Exotic</li>-->
+<!--                    </ul>-->
+<!--                    <p>-->
+<!--                        Prices will be customized depending on venues, vendors and accommodations.-->
+<!--                        </br></br>-->
+<!--                        Take our customized proposal preference test, so we can determine what your bride has in mind!-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 <?php include 'footer.php'; ?>
