a70bf643ba8c65c4bcc0cf21e9f4ae4bb99ad760
Erik Johnson <johnsone3745@gmail.com>
2013-09-09 21:04:51 -0600

    removed dead code, added social links

--- _header.php
+++ _header.php
@@ -3,31 +3,31 @@
 <div id="rox-social-bar" class="span3">
     <div class="row-fluid">
         <div class="catch-phrase">
-            <div id="rox-logo">
-                <img src="<?php echo ROOT?>img/logo.png"/>
-            </div>
 <!--            Create your perfect day, her perfect way-->
-<!--            <div class="pull-right">-->
-<!--                <a href="#">-->
-<!--                    <img alt="Facebook" title="Facebook" src="--><?php //echo ROOT?><!--img/glyphicons_410_facebook.png"/>-->
-<!--                </a>-->
-<!--                <a href="#">-->
-<!--                    <img alt="Twitter" title="Twitter" src="--><?php //echo ROOT?><!--img/glyphicons_411_twitter.png"/>-->
-<!--                </a>-->
-<!--                <a href="#">-->
-<!--                    <img alt="Instagram" title="Instagram" src="--><?php //echo ROOT?><!--img/glyphicons_412_instagram.png"/>-->
-<!--                </a>-->
-<!--                <a href="#">-->
-<!--                    <img alt="Pinterest" title="Pinterest" src="--><?php //echo ROOT?><!--img/glyphicons_399_pinboard.png"/>-->
-<!--                </a>-->
-<!--                <a href="#">-->
-<!--                    <img alt="LinkedIn" title="LinkedIn" src="--><?php //echo ROOT?><!--img/glyphicons_397_linked_in.png"/>-->
-<!--                </a>-->
-<!--            </div>-->
+
         </div>
-<!--        <div class="pull-right">-->
-<!--            -->
-<!--        </div>-->
+        <div id="rox-logo">
+            <img src="<?php echo ROOT?>img/logo.png"/>
+        </div>
+    </div>
+</div>
+<div class="span5">
+    <div class="">
+        <a href="#">
+            <img alt="Facebook" title="Facebook" src="<?php echo ROOT?>img/glyphicons_410_facebook.png"/>
+        </a>
+        <a href="#">
+            <img alt="Twitter" title="Twitter" src="<?php echo ROOT?>img/glyphicons_411_twitter.png"/>
+        </a>
+        <a href="#">
+            <img alt="Instagram" title="Instagram" src="<?php echo ROOT?>img/glyphicons_412_instagram.png"/>
+        </a>
+        <a href="#">
+            <img alt="Pinterest" title="Pinterest" src="<?php echo ROOT?>img/glyphicons_399_pinboard.png"/>
+        </a>
+        <a href="#">
+            <img alt="LinkedIn" title="LinkedIn" src="<?php echo ROOT?>img/glyphicons_397_linked_in.png"/>
+        </a>
     </div>
 </div>
 
